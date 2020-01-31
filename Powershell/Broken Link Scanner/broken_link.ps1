d0d260c6fd5521a5a4babd997bb44b5e280ab3ef
Saj <salimyasser@icloud.com>
2020-01-31 15:24:30 +0000

    Updated Broken Link Script

--- Powershell/Broken Link Scanner/broken_link.ps1
+++
@@ -1,9 +0,0 @@
-$URL = Read-Host -Prompt "URL"
-
-$HTTP_Request = [System.Net.WebRequest]::Create($URL)
-
-$HTTP_Response = $HTTP_Request.GetResponse()
-
-$HTTP_Status = [int]$HTTP_Response.StatusCode
-
-$HTTP_Status
