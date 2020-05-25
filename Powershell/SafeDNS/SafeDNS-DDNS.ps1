66204f77c8148866c97234bdfc6b4051c3b2ebe6
git-saj <salimyasser@icloud.com>
2020-05-25 21:31:57 +0100

    Created a module and added each relevent function

--- Powershell/SafeDNS/SafeDNS-DDNS.ps1
+++ Powershell/SafeDNS/SafeDNS-DDNS.ps1
@@ -1,3 +1,4 @@
+
 param (
     [Parameter(Mandatory=$true)]
     [string]
@@ -10,82 +11,11 @@ param (
     $APIKey
 
 )
+Import-Module .\SafeDNS-Module
+
 
 Function Get-CurrentIP {
     (Invoke-WebRequest ipv4.icanhazip.com).content.trim()
-}
-
-Function Get-SafeDNSRecord {
-    [cmdletbinding()]
-    Param (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Domain,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $DNSZone,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Type,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $APIKey
-    )
-
-    if ($APIKey) {
-        $headers = @{"Authorization"="$APIKey"}
-    }
-
-    $object = New-Object psobject
-
-    $object | Add-Member -MemberType NoteProperty -Name "Name" -Value $Domain
-    $object | Add-Member -MemberType NoteProperty -Name "Type" -Value $Type
-
-    $get_url = "https://api.ukfast.io/safedns/v1/zones/$DNSZone/records"
-
-    $body_get_record = @{
-        "name"="$Domain"
-        "type"="$Type"
-    }
-    
-    Invoke-RestMethod -Method Get -Uri $get_url -Headers $headers -Body $body_get_record -ContentType 'application/json'
-
-}
-
-Function New-SafeDNSRecord {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Domain,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $DNSZone,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Type,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Content,
-        [Parameter()]
-        [string]
-        $APIKey
-    )
-
-    if ($APIKey) {
-        $headers = @{"Authorization"="$APIKey"}
-    }
-
-    $post_url = "https://api.ukfast.io/safedns/v1/zones/$DNSZone/records"
-
-    $body_create_record = @{
-        "name"="$Domain"
-        "type"="$Type"
-        "content"="$Content"
-    } | ConvertTo-Json
-
-    Invoke-RestMethod -Method Post -Uri $post_url -Headers $headers -Body $body_create_record -ContentType 'application/json' | Out-Null
-    (Get-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -APIKey $APIKey).data
 }
 
 Function Compare-SafeDNSRecords {
@@ -106,58 +36,35 @@ Function Compare-SafeDNSRecords {
     }
 }
 
-Function Set-SafeDNSRecord {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Domain,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $DNSZone,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Type,
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Content,
-        [Parameter()]
-        [string]
-        $APIKey
-    )
-
-    if ($APIKey) {
-        $headers = @{"Authorization"="$APIKey"}
-    }
-
-    $patch_url = "https://api.ukfast.io/safedns/v1/zones/$DNSZone/records/$current_record_id"
-    $body_patch_record = @{
-        "name"="$Name"
-        "type"="$Type"
-        "content"="$Content"
-    } | ConvertTo-Json
-
-    Invoke-RestMethod -Method Patch -Uri $patch_url -Headers $headers -Body $body_patch_record -ContentType 'application/json' | Out-Null
-    (Get-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -APIKey $APIKey).data
-
-}
 
 $current_record = Get-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -APIKey $APIKey
 
 
-if (-not($current_record.data)) {
-    $current_record = New-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -Content (Get-CurrentIP) -APIKey $APIKey
-    (Get-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -APIKey $APIKey).data
+if (-not($current_record.id)) {
+    New-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -Content (Get-CurrentIP) -APIKey $APIKey
     exit
 } 
 
-$current_record_id = $current_record.data.id
-$current_record_content = $current_record.data.content
+
+if (($current_record | Measure-Object).count -gt 1) {
+    $i = 0
+    Write-Output "More than one records exist"
+    foreach ($cur in $current_record) {
+        if (-not($i -eq 0)) {
+            Remove-SafeDNSRecord -ID $cur.id -DNSZone $DNSZone -Type A -APIKey $APIKey
+        }
+        $i++
+    }
+    
+    
+}
+
+$current_record = Get-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -APIKey $APIKey
+
+$current_record_id = $current_record.id
+$current_record_content = $current_record.content
 $compare = Compare-SafeDNSRecords -ReferenceRecord $current_record_content -DifferenceRecord (Get-CurrentIP)
 
-
 if ($compare -eq $false) {
-    Set-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -Content (Get-CurrentIP) -APIKey $APIKey
-} else {
-    (Get-SafeDNSRecord -Domain $Domain -DNSZone $DNSZone -Type A -APIKey $APIKey).data
+    Set-SafeDNSRecord -ID $current_record_id -Domain $Domain -DNSZone $DNSZone -Type A -Content (Get-CurrentIP) -APIKey $APIKey
 }
